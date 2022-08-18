1a3da452c56e6f168581080c1524a1b5cdfa7010
dlgkssk38 <dlgkssk38@snu.ac.kr>
2022-08-18 09:47:33 +0900

    15656 clear

--- Baekjoon/[실버 III] 15656. N과 M (7).py
+++ Baekjoon/[실버 III] 15656. N과 M (7).py
@@ -1,5 +1,13 @@
+def Permutation(arr, n): # 중복순열 구현
+    for i in range(len(arr)):
+        if n == 1: # 원하는 r이 1이면
+            yield [arr[i]] # 하나만 뽑으면 됨
+        else:
+          # 하나 뽑고 나머지 r-1개에 대해서는 다시 permutation 돌리기
+            for next in Permutation(arr, n-1):
+                yield [arr[i]] + next
 N, M = map(int, input().split())
 num_list = list(map(int, input().split()))
-idx_list = [0] * M
-while True:
-    print(num_list)
+num_list.sort()
+for permu in Permutation(num_list, M):
+    print(*permu)
