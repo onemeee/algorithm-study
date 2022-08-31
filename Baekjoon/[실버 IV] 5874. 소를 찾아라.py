ef69d7bbaaec9df5e90f906ef21030afc72bffe7
dlgkssk38 <dlgkssk38@snu.ac.kr>
2022-08-31 22:05:18 +0900

    5874 clear

---
+++ Baekjoon/[실버 IV] 5874. 소를 찾아라.py
@@ -0,0 +1,22 @@
+infos = input()
+num_list = [0]
+cnt_1 = 0
+cnt_2 = 0
+for i in range(len(infos)-1):
+    if infos[i] == '(':
+        if infos[i+1] == '(':
+            cnt_1 += 1
+    else:
+        if infos[i] == ')':
+            if infos[i+1] == ')':
+                cnt_2 += 1
+            else:
+                if infos[i+1] == '(':
+                    if cnt_2:
+                        num_list.append(cnt_1*cnt_2 + num_list[-1])
+                        cnt_2 = 0
+if cnt_2:
+    num_list.append(cnt_1*cnt_2 + num_list[-1])
+print(num_list[-1])
+
+            
