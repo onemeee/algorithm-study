1db9c16648c554afd9b38682edbbc2057aff261e
dlgkssk38 <dlgkssk38@snu.ac.kr>
2022-09-27 14:11:49 +0900

    4881 시간초과

--- Fail/[D2] 4881. 배열 최소 합.py
+++ Fail/[D2] 4881. 배열 최소 합.py
@@ -1,19 +1,21 @@
-def tracking(arr, num):
-    if len(result) == n:
-        vals.append(sum(result))
-        return
-    for i in range(len(arr[num])):
-        result.append(arr[num][i])
-        num += 1
-        tracking(arr, num)
-        result.pop()
-        num -= 1
+def perm(arr, r):
+    for i in range(len(arr)):
+        if r == 1:
+            yield [arr[i]]
+        else:
+            for next in perm(arr[:i] + arr[i+1:], r-1):
+                yield [arr[i]] + next
 
-        
 for tc in range(1, int(input())+1):
     n = int(input())
     arr = [list(map(int, input().split())) for _ in range(n)]
-    result = []
-    vals = []
-    tracking(arr, 0)
-    print(f'#{tc} {min(vals)}')
+    order = [i for i in range(n)]
+    min_val = 500
+    for next in perm(order, n):
+        tmp_val = 0
+        for i in range(n):
+            tmp_val += arr[i][next[i]]
+            if tmp_val > min_val:
+                break
+        min_val = min(min_val, tmp_val)
+    print(f'#{tc} {min_val}')
