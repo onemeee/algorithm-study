a64f474a38c6d5c21f765d720545003c213fd128
dlgkssk38 <dlgkssk38@snu.ac.kr>
2022-09-30 11:16:53 +0900

    fail

--- SWEA/모의 SW 역량테스트/5648. 원자 소멸 시뮬레이션.py
+++ SWEA/모의 SW 역량테스트/5648. 원자 소멸 시뮬레이션.py
@@ -1,10 +1,22 @@
 from collections import deque
 for tc in range(1, int(input())+1):
     n = int(input())
-    que = deque(list(map(int, input().split())) for _ in range(n))
-    visited = [[0] * 2000 for _ in range(2000)]
+    que = deque()
+    for _ in range(n):
+        x, y, d, t = map(int, input().split())
+        x += 1000
+        y += 1000
+        que.append((x, y, d, t, 0))
+    visited = [[0] * 2001 for _ in range(2001)]
     
     while que:
-        x, y, d, k = que.popleft()
-        print(x, y, d, k)
+        x, y, d, k, t = que.popleft()
+        if d == 0:
+            pass
+        elif d == 1:
+            pass
+        elif d == 2:
+            pass
+        elif d == 3:
+            pass
     print(f'#{tc}')
