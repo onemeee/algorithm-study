fdf534183f479690f2f76ff98c769a8b19f13759
dlgkssk38 <dlgkssk38@snu.ac.kr>
2022-09-30 12:01:42 +0900

    2751 clear

--- Fail/[실버 V] 2751. 수 정렬하기 2.py
+++ Fail/[실버 V] 2751. 수 정렬하기 2.py
@@ -1,15 +1,21 @@
 import sys
 input = sys.stdin.readline
 
+# 카운팅 정렬 구현
 n = int(input())
-infos = [int(input())for _ in range(n)]
-for i in range(n):
-    min_val = infos[i]
-    min_idx = i
-    for j in range(i+1, n):
-        if infos[j] < min_val:
-            min_idx = j
-            min_val = infos[j]
-    infos[i], infos[min_idx] = infos[min_idx], infos[i]
-for info in infos:
-    print(info)
+nums = [int(input()) for _ in range(n)]
+m = max(nums)
+min_val = min(nums)
+k = 0
+if min_val < 0:
+    k = -min_val
+arr = [0] * (m+k+1)
+for num in nums:
+    arr[num+k] += 1
+result = []
+for i in range(len(arr)):
+    for j in range(arr[i]):
+        result.append(i-k)
+
+for re in result:
+    print(re)
