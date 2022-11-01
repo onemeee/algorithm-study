b087e6086e26bd01e1a74decf30af39b8ca5f80e
dlgkssk38 <dlgkssk38@snu.ac.kr>
2022-11-01 10:05:29 +0900

    1914...

---
+++ Fail/[실버 I] 1914. 하노이 탑.py
@@ -0,0 +1,13 @@
+n = int(input())
+
+def hanoi(n, a, b):
+    print(f'input: {n} {a} {b}')
+    if n > 1:
+        hanoi(n-1, a, 6-a-b)
+    print(a, b)
+    if n > 1:
+        hanoi(n-1, 6-a-b, b) 
+
+print(2**n-1)
+if n <= 20:
+    hanoi(n, 1, 3)
