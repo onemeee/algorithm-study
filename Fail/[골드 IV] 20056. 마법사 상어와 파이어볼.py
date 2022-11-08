e471057e412e7a65fddc8af8dafc7950c01f13b0
dlgkssk38 <dlgkssk38@snu.ac.kr>
2022-11-09 00:05:41 +0900

    좀만 더 고민!

--- Fail/[골드 IV] 20056. 마법사 상어와 파이어볼.py
+++ Fail/[골드 IV] 20056. 마법사 상어와 파이어볼.py
@@ -1,25 +1,51 @@
 import sys
-from collections import deque
 input = sys.stdin.readline
 
-def fire(r, c, mg, s, d):
+def fire(locs, k):
     global n
     dires = [(-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1)]
-    q = deque()
-    q.append((r, c, mg, s, d))
-    while q:
-        r, c, mg, s, d = q.popleft()
-        nr, nc = r+dires[d][0]*s, c+dires[d][1]*s
-        grid[nr][nc] += mg
-        visited[nr][nc] += 1
-        if visited[nr][nc] >= 2:
-            if visited[nr][nc] % 2:
-                pass
+    visited = [[0]*n for _ in range(n)]
+    check = [[0]*n for _ in range(n)]
+    fast = [[0]*n for _ in range(n)]
+    # 행, 열, 질량, 속력, 방향
+    for _ in range(k):
+        for r, c, m, s, d in locs:
+            nr, nc = r+dires[d][0]*s, c+dires[d][1]*s
+            grid[nr][nc] += m
+            visited[nr][nc] += 1
+            fast[nr][nc] += s
+            if not check[nr][nc]:
+                if d%2:
+                    check[nr][nc] = 1
+                else:
+                    check[nr][nc] = 2
+            else:
+                if check[nr][nc]%2 and d%2:
+                    pass
+                elif not check[nr][nc]%2 and not d%2:
+                    pass
+                else:
+                    check[nr][nc] = 0
+        locs = []
+        for i in range(n):
+            for j in range(n):
+                if visited[i][j] >= 2:
+                    if check[i][j]:
+                        for nd in range(0, 7, 2):
+                            locs.append((i, j, grid[i][j]//5, fast[i][j]//visited[i][j], nd))
+                    else:
+                        for nd in range(1, 8, 2):
+                            locs.append((i, j, grid[i][j]//5, fast[i][j]//visited[i][j], nd))
+                    check[i][j] = 0
+                    visited[i][j] = 0
+                    fast[i][j] = 0
 
 n, m, k = map(int, input().split())
 grid = [[0]*n for _ in range(n)]
-visited = [[0]*n for _ in range(n)]
+locs = []
 for _ in range(m):
-    # 행, 열, 질량, 속력, 방향
-    r, c, mg, s, d = map(int, input().split())
+    r, c, m, s, d = map(int, input().split())
+    locs.append((r-1, c-1, m, s, d)) 
+fire(locs, k)
+print(grid)
     
