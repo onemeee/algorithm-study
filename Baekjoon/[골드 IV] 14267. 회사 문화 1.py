5c8a6041c3048e23d63c3801a48e550d701648cf
dlgkssk38 <dlgkssk38@snu.ac.kr>
2022-12-13 16:50:29 +0900

    14267 clear

--- Baekjoon/[골드 IV] 14267. 회사 문화 1.py
+++ Baekjoon/[골드 IV] 14267. 회사 문화 1.py
@@ -1,19 +1,16 @@
 import sys
-from collections import deque
-input = sys.stdin.readline
 
 def praise():
+    input = sys.stdin.readline
     n, m = map(int, input().split())
     workers = [0] * n
-    superior = list(map(int, input().rstrip().split()))
+    superior = list(map(int, input().split()))
     for _ in range(m):
         i, j = map(int, input().split())
         workers[i-1] += j
 
-    for i in range(n):
-        if superior[i] == -1:
-            continue
-        workers[i] += workers[superior[i]-1]
+    for l in range(1, n):
+        workers[l] += workers[superior[l]-1]
     print(*workers)
 
 praise()
