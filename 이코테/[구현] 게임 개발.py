a138c7c1b3d3fa775fd0f3799f31daf62b2f3793
dlgkssk38 <dlgkssk38@snu.ac.kr>
2023-02-16 01:09:43 +0900

    구현 문제풀이 완료

--- 이코테/[구현] 게임 개발.py
+++ 이코테/[구현] 게임 개발.py
@@ -7,17 +7,39 @@ a, b, d = map(int, input().split())
 maps = [list(map(int, input().rstrip().split())) for _ in range(n)]
 
 moves = [(-1, 0), (0, 1), (1, 0), (0, -1)]
-result = 0
-while True:
-    # 왼쪽 방향 바라보기
+
+# 왼쪽으로 회전
+def turn_left():
+    global d
     d -= 1
     if d == -1:
         d = 3
-    a += moves[d][0]
-    b += moves[d][1]
-    if 0 <= a < n and 0 <= b < m:
-        if maps[a][b] == 0:
-            result += 1
-            maps[a][b] = 2
-        else:
-            pass
+
+count = 1 # 현재 위치 포함
+turn_count = 0
+
+while True:
+    if turn_count == 4:
+        # 뒤로 갈 방향
+        d += 2
+        d %= 4
+        a -= moves[d][0]
+        b -= moves[d][1]
+        turn_count = 1
+        if maps[a][b] == 1:
+            break
+
+    # 왼쪽으로 회전
+    turn_left()
+    turn_count += 1
+
+    # 회전하고 직전한 경우
+    na, nb = a + moves[d][0], b + moves[d][1]
+    if 0 <= na < n and 0 <= nb < m:
+        # 방문하지 않은 경우
+        if not maps[na][nb]:
+            count += 1
+            maps[na][nb] = 2
+            a, b = na, nb
+            turn_count = 0
+print(count)
