763b500a5c2901bd1a493b7b89e6f0d269d66044
dlgkssk38 <dlgkssk38@snu.ac.kr>
2023-02-21 21:47:37 +0900

    1138 clear

--- Baekjoon/실버/[실버 II] 1138. 한 줄로 서기.py
+++ Baekjoon/실버/[실버 II] 1138. 한 줄로 서기.py
@@ -3,4 +3,14 @@ infos = list(map(int, input().split()))
 orders = [0] * n
 
 for i in range(n):
-    pass
+    big = infos[i]
+    for j in range(n):
+        if orders[j] > i:
+            big -= 1
+        elif orders[j] == 0:
+            if big <= 0:
+                orders[j] = i+1
+                break
+            big -= 1
+
+print(*orders)
